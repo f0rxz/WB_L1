afb964cfbb5962b5e20046a5957e9c95a7df9f1f
forxz <fast35890@gmail.com>
2025-11-02 00:45:49 +0300

    l1.6

--- l1.6/main.go
+++ l1.6/main.go
@@ -1,12 +1,68 @@
-/*Остановка горутины
-Реализовать все возможные способы остановки выполнения горутины.
-
-Классические подходы: выход по условию, через канал уведомления, через контекст, прекращение работы runtime.Goexit() и др.
-
-Продемонстрируйте каждый способ в отдельном фрагменте кода.*/
-
 package main
 
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"time"
+)
+
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func(ctx context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("goroutine 1 stopped by context")
+				return
+			default:
+				fmt.Println("goroutine 1")
+				time.Sleep(time.Millisecond * 200)
+			}
+		}
+	}(ctx)
+
+	go func() {
+		i := 0
+		for {
+			fmt.Println("goroutine 2")
+			time.Sleep(time.Millisecond * 200)
+			i++
+			if i == 5 {
+				fmt.Println("goroutine 2 stopped by condition")
+				return
+			}
+		}
+	}()
+
+	done := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				fmt.Println("goroutine 3 stopped by channel")
+				return
+			default:
+				fmt.Println("goroutine 3")
+				time.Sleep(time.Millisecond * 200)
+			}
+		}
+	}()
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			fmt.Println("goroutine 4")
+		}
+		fmt.Println("goroutine 4 stopped by runtime")
+		runtime.Goexit()
+	}()
+
+	time.Sleep(time.Second * 1)
+	cancel()
+	close(done)
+	time.Sleep(time.Second)
+	fmt.Println("main goroutine exit")
 
 }
